17ee47532d67a6234f892bf41a2a667dfb079ce4
SlavePejkov <119795961+SlavePejkov@users.noreply.github.com>
2023-03-17 20:44:54 +0100

    Add files via upload

---
+++ Class03_Homework/StudentGroup/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace StudentGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string[] studentsG1 = new string[5] { "Slave", "Marko", "Darko", "Sarko", "Anja" };
+            string[] studentsG2 = new string[5] { "Ivan", "Mile", "Mia", "Andrea", "Goran" };
+
+            Console.WriteLine("Please enter the numbers 1 or 2");
+            string usersGroup = Console.ReadLine();
+
+            int usersInputedNumber;
+            bool convertedNumber = int.TryParse(usersGroup, out usersInputedNumber);
+
+            if (!convertedNumber)
+            {
+                Console.WriteLine("Please enter a valid number");
+            }else if(usersInputedNumber > 2 || usersInputedNumber < 1)
+            {
+                Console.WriteLine("Please enter the correct number");
+            }
+            
+            if(usersInputedNumber == 1)
+            {
+                Console.WriteLine("The students in G1 are:");
+
+                foreach (string name in studentsG1)
+                {
+                    Console.WriteLine(name);
+                }
+            }else if(usersInputedNumber == 2)
+            {
+                Console.WriteLine("The students in G2 are:");
+
+                foreach (string name in studentsG2)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
---
+++ Class03_Homework/SumOfEven/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace SumOfEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter some numbers");
+            
+
+            string input1 = Console.ReadLine();
+            int intInputOne = int.Parse(input1);
+            
+
+            string input2 = Console.ReadLine();
+            int intInputTwo = int.Parse(input2);
+            
+
+            string input3 = Console.ReadLine();
+            int intInputThree = int.Parse(input3);
+            
+
+            string input4 = Console.ReadLine();
+            int intInputFour = int.Parse(input4);
+           
+
+            string input5 = Console.ReadLine();
+            int intInputFive = int.Parse(input5);
+            
+
+            string input6 = Console.ReadLine();
+            int intInputSix = int.Parse(input6);
+            
+
+          
+
+
+            int sum = 0;
+            int[] sumOfEven = new int[6] { intInputOne, intInputTwo, intInputThree, intInputFour, intInputFive, intInputSix };
+
+            for (int i = 0; i < sumOfEven.Length; i++)
+            {
+                if (sumOfEven[i] % 2 == 0)
+                {
+                    sum += sumOfEven[i];
+                }
+            }
+            Console.WriteLine("The result is " + sum);
+
+        }
+    }
+}
